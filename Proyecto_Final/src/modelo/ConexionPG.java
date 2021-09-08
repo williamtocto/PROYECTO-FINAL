5795bea018bb5c19edd541bd20ef96da82720375
WILLIAM TOCTO <user.name>
2021-09-08 11:22:48 -0500

    Clase Conexion a la Base de Datos

---
+++ Proyecto_Final/src/modelo/ConexionPG.java
@@ -0,0 +1,75 @@
+
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Patricio
+ */
+public class ConexionPG {
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+    //Datos de su conexion local:
+    private String cadenaConexion="jdbc:postgresql://localhost:5432/persona";
+    private String usuarioPG="postgres";
+    private String contrasPG="1234";
+
+    public ConexionPG() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            //No encontramos driver
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+           con= DriverManager.getConnection(cadenaConexion, usuarioPG, contrasPG);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public ResultSet consulta(String sqlc){
+    
+        try {
+            st= con.createStatement();
+            ResultSet rs=st.executeQuery(sqlc);
+            return rs;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public boolean accion(String sqla){
+        try {
+            st=con.createStatement();
+            boolean rb=st.execute(sqla);
+            st.close();
+            return true; //rb;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+    
+    
+    
+}
