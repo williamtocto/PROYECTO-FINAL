6a770000e9ffee1741568382e2437e8c955bf9e1
WILLIAM TOCTO <william.tocto.est@tecazuay.edu.ec>
2021-09-17 01:33:43 -0500

    Ventana Principal y actulizacion control usuario

--- Proyecto_Final/src/modelo/Modelo_Rol.java
+++ Proyecto_Final/src/modelo/Modelo_Rol.java
@@ -5,11 +5,11 @@ package modelo;
 public class Modelo_Rol {
 
     ConexionPG pg=new ConexionPG();
-    Rol r=new Rol();
+    //Rol r=new Rol();
     
     public void grabarRol() {
         
-        String sql=  "INSERT INTO rol(tipo_rol) VALUES('" + r.getTipo_rol() + "')";
+       // String sql=  "INSERT INTO rol(tipo_rol) VALUES('" + r.getTipo_rol() + "')";
         
         
         
