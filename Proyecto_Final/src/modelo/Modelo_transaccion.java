3b00438a11b6f7c5563181be310540851ce2a5a2
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2021-09-25 21:35:30 -0500

    EDICION EN MODELO_TRANSACCION

--- Proyecto_Final/src/modelo/Modelo_transaccion.java
+++ Proyecto_Final/src/modelo/Modelo_transaccion.java
@@ -5,6 +5,18 @@ package modelo;
  *
  * @author Mateo
  */
-public class Modelo_transaccion {
+public class Modelo_transaccion extends Transaccion {
+
+    public Modelo_transaccion(int cod_transaccion, String tipo_transaccion, int codigo_socio, String fecha_trans, double monto, double saldo) {
+        super(cod_transaccion, tipo_transaccion, codigo_socio, fecha_trans, monto, saldo);
+    }
+
+    public Modelo_transaccion() {
+    }
+    
+    
+    
+    
+    
     
 }
