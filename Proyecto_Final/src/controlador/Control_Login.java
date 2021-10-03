06257478c7313285d1e4de72599d0f9eddf50358
WILLIAM TOCTO <william.tocto.est@tecazuay.edu.ec>
2021-10-03 09:48:00 -0500

    Actualizacion de vista privilegios con su modelo y controlador

--- Proyecto_Final/src/controlador/Control_Login.java
+++ Proyecto_Final/src/controlador/Control_Login.java
@@ -1,4 +1,3 @@
-
 package controlador;
 
 import Vista.Vista_Login;
@@ -6,38 +5,41 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import modelo.Modelo_Login;
-import modelo.Modelo_Usuario;
-import modelo.Usuario;
 import vista.Vista_Principal;
 
 public class Control_Login {
-    
-    private Modelo_Usuario modelo;
+
+    private Modelo_Login modelo;
     private Vista_Login vista;
-    
+
+    public Control_Login(Modelo_Login modelo, Vista_Login vista) throws SQLException {
+        this.modelo = modelo;
+        this.vista = vista;
+        vista.setTitle("Inicio");
+        vista.setVisible(true);
+
+    }
+
     public void validarUsuario() throws SQLException {
 
         String usuario = vista.getTxtUsuari().getText();
         String clave = vista.getTxt_Password().getText();
-        Usuario usu = new Usuario();
-        Modelo_Login m= new Modelo_Login();
-        m.Usuario(usuario, clave);
-        if (usu.getCodig_rol() != 0) {
-            Vista_Principal ven = new Vista_Principal(usu.getCodig_rol());
+        modelo.setUsuario(usuario);
+        modelo.setContrasenia(clave);
+        int codigo = modelo.ValidarUsuario();
+        if (codigo != 0) {
+            Vista_Principal ven = new Vista_Principal(codigo);
         }
     }
 
-    
     public void IniciaControl() {
-        
-           vista.getbtnIngresar().addActionListener(l -> {
+        vista.getbtnIngresar().addActionListener(l -> {
             try {
                 validarUsuario();
             } catch (SQLException ex) {
                 Logger.getLogger(Control_Login.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
-        
     }
-    
+
 }
--- Proyecto_Final/src/modelo/Modelo_Login.java
+++ Proyecto_Final/src/modelo/Modelo_Login.java
@@ -10,12 +10,11 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import vista.Vista_Principal;
 
+public class Modelo_Login extends Usuario {
 
-public class Modelo_Login extends Usuario{
-    
-        ConexionPG con=new ConexionPG();
-    
-    public void Usuario(String usuario, String clave) throws SQLException {
+    ConexionPG con = new ConexionPG();
+
+   /* public void Usuario(String usuario, String clave) throws SQLException {
 
         String sql = "select * from usuario where usuario='" + usuario + "' and contrasenia='" + clave + "'";
         try (ResultSet rs = con.consulta(sql)) {
@@ -28,9 +27,23 @@ public class Modelo_Login extends Usuario{
             rs.close();
         }
 
+    }*/
+
+    public int ValidarUsuario() throws SQLException {
+
+        String sql = "select * from usuario where usuario='" + getUsuario() + "' and contrasenia='" + getContrasenia() + "'";
+        System.out.println(getUsuario()+getContrasenia());   
+        ResultSet rs = con.consulta(sql);      
+        if (rs.next()) {
+            System.out.println("Existe la consultaaaaaaaaaaaaaaaaaaaaaaaaa");
+            System.out.println(rs.getInt("codigo_rol"));
+            return rs.getInt("codigo_rol");          
+        } else {
+            JOptionPane.showMessageDialog(null, "ERROR USUARIO NO REGISTRADO", "", 2);
+        }
+        rs.close();
+        return 0;
     }
-    
-    public void ValidarUsuario() {
-   
-    }
+
+
 }
