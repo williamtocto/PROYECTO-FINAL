5226b72ca1094eab899390e2d2675aaa939a4648
WILLIAM TOCTO <william.tocto.est@tecazuay.edu.ec>
2021-10-15 00:27:19 -0500

    Revert "Validaciones Vista Principal"
    This reverts commit 2513b9f8d10d7b89e0d0902e824c9622c6a0f744.

--- Proyecto_Final/src/modelo/Modelo_Login.java
+++ Proyecto_Final/src/modelo/Modelo_Login.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package modelo;
 
 import java.sql.ResultSet;
@@ -8,19 +12,22 @@ import vista.Vista_Principal;
 
 public class Modelo_Login extends Usuario {
 
+    ConexionPG con = new ConexionPG();
+
     public int ValidarUsuario() throws SQLException {
-        ConexionPG con = new ConexionPG();
+
         String sql = "select * from usuario where usuario='" + getUsuario() + "' and contrasenia='" + getContrasenia() + "'";
-        System.out.println(getUsuario() + getContrasenia());
-        ResultSet rs = con.consulta(sql);
+        System.out.println(getUsuario()+getContrasenia());   
+        ResultSet rs = con.consulta(sql);      
         if (rs.next()) {
             System.out.println(rs.getInt("codigo_rol"));
-            return rs.getInt("codigo_rol");
+            return rs.getInt("codigo_rol");          
         } else {
             JOptionPane.showMessageDialog(null, "ERROR USUARIO NO REGISTRADO", "", 2);
         }
         rs.close();
         return 0;
     }
+
 
 }
