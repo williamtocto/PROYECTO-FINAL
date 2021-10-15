2513b9f8d10d7b89e0d0902e824c9622c6a0f744
WILLIAM TOCTO <william.tocto.est@tecazuay.edu.ec>
2021-10-15 00:27:02 -0500

    Validaciones Vista Principal

--- Proyecto_Final/src/modelo/Modelo_privilegios.java
+++ Proyecto_Final/src/modelo/Modelo_privilegios.java
@@ -9,8 +9,6 @@ import java.util.logging.Logger;
 
 public class Modelo_privilegios extends Privilegios {
 
-    ConexionPG con = new ConexionPG();
-
     public Modelo_privilegios() {
     }
 
@@ -19,23 +17,27 @@ public class Modelo_privilegios extends Privilegios {
     }
 
     public boolean grabarPrivilegios() {
+        ConexionPG con = new ConexionPG();
         String sql = "INSERT INTO relacion_rol_opciones (cod_rol,cod_opcion,estado_rol) "
                 + "VALUES (" + getCod_rol() + ", " + getCod_opcion() + ", '" + getEstado_rol() + "');";
         return con.accion(sql);
     }
 
     public boolean modificarPrivilegios() {
+        ConexionPG con = new ConexionPG();
         String sql = "UPDATE relacion_rol_opciones SET  estado_rol= '" + getEstado_rol()
                 + "' WHERE cod_rol=" + getCod_rol() + "and cod_opcion= " + getCod_opcion() + ";";
         return con.accion(sql);
     }
 
     public boolean EliminarPrivilegios() {
+        ConexionPG con = new ConexionPG();
         String sql = "delete from relacion_rol_opciones where cod_rol=" + getCod_rol() + ";";
         return con.accion(sql);
     }
 
     public List<Privilegios> CargarLista() throws SQLException {
+        ConexionPG con = new ConexionPG();
         String sql = "select * from relacion_rol_opciones where cod_rol= " + getCod_rol();
         ResultSet rs = con.consulta(sql);
         List<Privilegios> pr = new ArrayList<>();
@@ -49,9 +51,10 @@ public class Modelo_privilegios extends Privilegios {
     }
 
     public int verificarPrivilegios(String rol) {
+        ConexionPG con = new ConexionPG();
         int cod = 0;
         try {
-            String sql = "Select * from rol where tipo_rol= '" + rol+"'";
+            String sql = "Select * from rol where tipo_rol= '" + rol + "'";
             ResultSet rs = con.consulta(sql);
             while (rs.next()) {
                 cod = rs.getInt("codigo_rol");
@@ -65,7 +68,7 @@ public class Modelo_privilegios extends Privilegios {
                     c = rs1.getInt("cod_rol");
                 }
                 return c;
-            }else{
+            } else {
                 return 0;
             }
 
